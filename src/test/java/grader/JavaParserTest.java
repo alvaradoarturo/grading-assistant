07b1ed943913632c3f751bba7c795a11c2db1865
alvaradoarturo <aalva058@ucr.edu>
2025-07-07 10:41:36 -0700

    Added test cases for classes using findAll and getClassByName

--- src/test/java/grader/JavaParserTest.java
+++ src/test/java/grader/JavaParserTest.java
@@ -2,8 +2,10 @@ package grader;
 
 import grader.parser.*;
 import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
 import com.github.javaparser.ast.body.MethodDeclaration;
 import java.util.List;
+import java.util.Optional;
 import com.github.javaparser.StaticJavaParser;
 import com.github.javaparser.ast.Node;
 
@@ -28,5 +30,35 @@ public class JavaParserTest {
                 foundMethod = true;
         }
         assertTrue(foundMethod);
+    }
+
+    @Test
+    public void shouldHaveClass() {
+        boolean foundClass = false;
+        String code = "public class Test { void hellur() { System.out.println(\"Hello World\"); } }";
+        Parser parser = new JavaParser();
+        AST ast = parser.parse(code);
+
+        // gives class & interfaces
+        List<ClassOrInterfaceDeclaration> classes = ast.getRoot().findAll(ClassOrInterfaceDeclaration.class);
+
+        for (ClassOrInterfaceDeclaration singleClass : classes) {
+            if (!singleClass.isInterface() && singleClass.getNameAsString().equals("Test")) {
+                foundClass = true;
+            }
+        }
+        assertTrue(foundClass);
+    }
+
+    @Test
+    public void shouldHaveClasssTwo() {
+        // test uses getClassByName instead
+        boolean foundClass = false;
+        String code = "public class Test { void hellur() { System.out.println(\"Hello World\"); } }";
+        Parser parser = new JavaParser();
+        AST ast = parser.parse(code);
+
+        Optional<ClassOrInterfaceDeclaration> studentClass = ast.getRoot().getClassByName("Test");
+        assertTrue(studentClass.isPresent());
     }
 }
