350c37d47836f33ba0a53edaa21b92d8960f0dcc
alvaradoarturo <aalva058@ucr.edu>
2025-07-31 20:33:37 -0700

    Updated Array Usage Config file

--- src/main/java/grader/analyzer/ArrayUsageConfig.java
+++ src/main/java/grader/analyzer/ArrayUsageConfig.java
@@ -1,5 +1,11 @@
 package grader.analyzer;
 
 public class ArrayUsageConfig {
+    public boolean requireArrayDeclaration = false;
+    public String requireInitialization = "";
+    public boolean requireArrayAcess = false;
+    public boolean requireArrayLengthUsage = false;
+    public boolean requireLoopOverArray = false;
+    public boolean checkHardCodedValues = false;
 
 }
