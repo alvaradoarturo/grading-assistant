c300dda9715df099ba1e2885a113d01db0765a62
alvaradoarturo <aalva058@ucr.edu>
2025-08-01 18:42:43 -0700

    Added Test Case to check for array declration

--- src/test/java/grader/ArrayAnalyzerTest.java
+++ src/test/java/grader/ArrayAnalyzerTest.java
@@ -18,7 +18,7 @@ import grader.parser.Parser;
 
 public class ArrayAnalyzerTest {
     @Test
-    public void checkForDeclaredArray() throws IOException {
+    public void checkForMissingDeclaredArray() throws IOException {
         String code = FileUtil.readStudentCode(
                 "/Users/theboy/Desktop/thesis/grading-assistant/grading-assistant/src/main/StudentSamples/StudentSample2.java");
         Parser parser = new JavaParser();
@@ -36,5 +36,25 @@ public class ArrayAnalyzerTest {
 
         assertTrue(result.getScore() < 1);
         assertTrue(result.getDescription().contains("Declaration Not Found"));
+    }
+
+    @Test
+    public void checkForDeclaredArray() throws IOException {
+        String code = FileUtil.readStudentCode(
+                "/Users/theboy/Desktop/thesis/grading-assistant/grading-assistant/src/main/StudentSamples/StudentSample.java");
+        Parser parser = new JavaParser();
+        AST ast = parser.parse(code);
+
+        // Custom Figuration
+        ArrayConfig config = new ArrayConfig();
+        config.requireArrayDeclaration = true;
+
+        ArrayAnalyzer analyzer = new ArrayAnalyzer(config);
+        AnalyzerResult result = analyzer.analyze(ast);
+
+        System.out.println("Score: " + result.getScore());
+        System.out.println("Result: " + result.getDescription());
+
+        assertTrue(result.getDescription().contains("Declaration Found"));
     }
 }
