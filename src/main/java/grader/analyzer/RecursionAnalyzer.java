e4856bddb0614c62f9f631ddf7b27f3823b38fe8
alvaradoarturo <aalva058@ucr.edu>
2025-08-02 21:52:03 -0700

    Added implementation for finding if recursion is implemented

--- src/main/java/grader/analyzer/RecursionAnalyzer.java
+++ src/main/java/grader/analyzer/RecursionAnalyzer.java
@@ -1,5 +1,12 @@
 package grader.analyzer;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.body.MethodDeclaration;
+import com.github.javaparser.ast.expr.MethodCallExpr;
+
 import grader.parser.AST;
 
 public class RecursionAnalyzer implements Analyzer {
@@ -10,7 +17,31 @@ public class RecursionAnalyzer implements Analyzer {
     }
 
     public AnalyzerResult analyze(AST ast) {
-        return new AnalyzerResult("", 0);
+        CompilationUnit cu = ast.getRoot();
+        List<String> feedback = new ArrayList<>();
+        int score = 0;
+
+        // get all methods
+        List<MethodDeclaration> methods = cu.findAll(MethodDeclaration.class);
+
+        if (configuration.recursionRequired) {
+            boolean recurionFound = false;
+            for (MethodDeclaration method : methods) {
+                String methodName = method.getNameAsString();
+                // get a list of all methods called inside of each declared method
+                List<MethodCallExpr> methodCalls = method.findAll(MethodCallExpr.class);
+                for (MethodCallExpr methodCall : methodCalls) {
+                    if (methodCall.getNameAsString().equals(methodName)) {
+                        feedback.add("Recursion Found");
+                        recurionFound = true;
+                    }
+                }
+            }
+            if (!recurionFound) {
+                feedback.add("Recursion Not Found");
+            }
+        }
+        return new AnalyzerResult((String.join("/n", feedback)), score);
     }
 
 }
