17b90896e683b2a4ff82861137fe1387bb782414
alvaradoarturo <aalva058@ucr.edu>
2025-08-02 21:57:27 -0700

    Added Test for finding recurion implementation

---
+++ src/test/java/grader/RecursionAnalyzerTest.java
@@ -0,0 +1,38 @@
+package grader;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.io.IOException;
+import java.util.Arrays;
+
+import org.junit.jupiter.api.Test;
+
+import grader.analyzer.AnalyzerResult;
+import grader.analyzer.LoopAnalyzer;
+import grader.analyzer.LoopAnalyzerConfig;
+import grader.analyzer.RecursionAnalyzer;
+import grader.analyzer.RecursionConfig;
+import grader.parser.AST;
+import grader.parser.JavaParser;
+import grader.parser.Parser;
+
+public class RecursionAnalyzerTest {
+    @Test
+    public void checksForLoops() throws IOException {
+        String code = FileUtil.readStudentCode(
+                "/Users/theboy/Desktop/thesis/grading-assistant/grading-assistant/src/main/StudentSamples/StudentSample.java");
+        Parser parser = new JavaParser();
+        AST ast = parser.parse(code);
+
+        // Custom Figuration
+        RecursionConfig config = new RecursionConfig();
+        config.recursionRequired = true;
+
+        RecursionAnalyzer analyzer = new RecursionAnalyzer(config);
+        AnalyzerResult result = analyzer.analyze(ast);
+
+        System.out.println("Result For Recursion Test: " + result.getDescription());
+
+        assertTrue(result.getDescription().contains("Recursion Found"));
+    }
+}
