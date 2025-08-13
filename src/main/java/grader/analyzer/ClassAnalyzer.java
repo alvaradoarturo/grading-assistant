bfc42aaf944d6c39fa7ff56bb867391b1bc381f9
alvaradoarturo <aalva058@ucr.edu>
2025-08-12 17:00:20 -0700

    Changes to class analyzer

--- src/main/java/grader/analyzer/ClassAnalyzer.java
+++ src/main/java/grader/analyzer/ClassAnalyzer.java
@@ -1,11 +1,14 @@
 package grader.analyzer;
 
+import java.lang.reflect.Constructor;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
 
 import com.github.javaparser.ParseProblemException;
 import com.github.javaparser.ast.CompilationUnit;
 import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
+import com.github.javaparser.ast.body.ConstructorDeclaration;
 
 import grader.analyzer.configs.ClassConfig;
 import grader.parser.AST;
@@ -23,6 +26,7 @@ public class ClassAnalyzer implements Analyzer {
         int score = 0;
         CompilationUnit cu = ast.getRoot();
 
+        // see if name of class is correct
         if (configuration.requiredClassName != null) {
             // get all class Declarations
             boolean foundClass = false;
@@ -37,6 +41,12 @@ public class ClassAnalyzer implements Analyzer {
                 feedback.add("Class: " + configuration.requiredClassName + " not found");
             }
         }
+        // see if signature of constructor(s) is correct
+        if (!configuration.requiredConstructors.isEmpty()) {
+            List<ConstructorDeclaration> constructors = cu.findAll(ConstructorDeclaration.class);
+            
+        }
+
         return new AnalyzerResult((String.join("\n", feedback)), score);
     }
 }
--- src/main/java/grader/analyzer/configs/ClassConfig.java
+++ src/main/java/grader/analyzer/configs/ClassConfig.java
@@ -1,5 +1,9 @@
 package grader.analyzer.configs;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ClassConfig {
     public String requiredClassName = null;
+    public List<List<String>> requiredConstructors = new ArrayList<>();
 }
