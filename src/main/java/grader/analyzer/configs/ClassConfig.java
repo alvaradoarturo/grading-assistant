dcbaf7e73861b1c0525339f55ac795b22ec75f7b
alvaradoarturo <aalva058@ucr.edu>
2025-08-13 22:09:19 -0700

    Modified class configuration file for required field types

--- src/main/java/grader/analyzer/configs/ClassConfig.java
+++ src/main/java/grader/analyzer/configs/ClassConfig.java
@@ -4,6 +4,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ClassConfig {
+    // class name
     public String requiredClassName = null;
+
+    // Correct constructor header
     public List<List<String>> requiredConstructors = new ArrayList<>();
+
+    // Declares appropriate instance variables
+    public List<String> requiredFieldTypes = new ArrayList<>();
 }
