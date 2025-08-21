80ae18019d46785477a4d038e6c3122ab3c64df2
alvaradoarturo <aalva058@ucr.edu>
2025-08-20 23:36:11 -0700

    Updated Config file for method signature analyzer

--- src/main/java/grader/analyzer/configs/MethodSignatureConfig.java
+++ src/main/java/grader/analyzer/configs/MethodSignatureConfig.java
@@ -1,5 +1,13 @@
 package grader.analyzer.configs;
 
 public class MethodSignatureConfig {
+    public java.util.List<RequiredMethod> requiredMethods = java.util.List.of();
 
+    public static class RequiredMethod {
+        public String name;
+        public String returnType;
+        public java.util.List<String> paramTypes;
+        public boolean requirePublic = true;
+        public boolean requireStatic = false;
+    }
 }
