43a5fee90d63c7195f423ea273866658a7bfedce
alvaradoarturo <aalva058@ucr.edu>
2025-09-26 22:56:34 -0700

    Decision Decision: only one student file will exist per student

--- src/main/java/grader/runner/JUnitTestRunner.java
+++ src/main/java/grader/runner/JUnitTestRunner.java
@@ -3,6 +3,7 @@ package grader.runner;
 import java.io.File;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
@@ -20,21 +21,36 @@ import grader.model.PointResult;
  */
 
 public class JUnitTestRunner implements GradingTestRunner {
-    private Path studentSource;
-    private List<String> testClassNames;
-    private Path studentOutputDirectory;
+    private Path studentSourceFile;
+    private String testClassLoc;
+    private Path outDir = Paths.get("target/student-classes");
+    private Path testsDir = Paths.get("target/test-classes");
 
-    public JUnitTestRunner(Path studentSource, List<String> testClassNames, Path studentOutputDirectory) {
-        this.studentSource = Objects.requireNonNull(studentSource, "studentSource");
-        this.testClassNames = List.copyOf(Objects.requireNonNull(testClassNames, "testClassNames"));
-        this.studentOutputDirectory = Objects.requireNonNull(studentOutputDirectory, "studentOutputDirectory");
-    }
-
-    public JUnitTestRunner(Path studentSource, List<String> testClassNames) {
-        this(studentSource, testClassNames, Paths.get("target/student-classes"));
+    public JUnitTestRunner(Path studentSourceFile, String testClassLoc) {
+        this.studentSourceFile = Objects.requireNonNull(studentSourceFile, "studentSourceFile");
+        this.testClassLoc = Objects.requireNonNull(testClassLoc, "testClassLoc");
     }
 
     @Override
     public List<PointResult> runTests() {
+        List<PointResult> points = new ArrayList<>();
+        // Compile student code (file most likely or directory) to
+        // target/student-classes
+        CompileResult compileResult = compileSingleFile(studentSourceFile, outDir);
+        return points;
+    }
+
+    private class CompileResult {
+        final boolean success;
+        final List<String> diagnostics;
+
+        CompileResult(boolean sucess, List<String> diagnostics) {
+            this.success = sucess;
+            this.diagnostics = diagnostics;
+        }
+    }
+
+    private static CompileResult compileSingleFile(Path sourceFile, Path out) {
+        List<String> diagnostics = new ArrayList<>();
     }
 }
